975b637238cef4e8d068a9ab21894ebd7cdb7316
Tibo Smet <74244876+TiboSmetKdG@users.noreply.github.com>
2021-01-06 16:08:12 +0100

    Add files via upload

---
+++ Main/LED_Matrix_Test.ino
@@ -0,0 +1,23 @@
+/*
+    LED Matrix testing
+
+    Program to test the LED Matrix that we will be using for our project.
+
+    The circuit:
+    * LED Matrix
+
+    Created 06 January 2021
+    By Tibo Smet
+    Modified 06 January 2021
+    By Tibo Smet
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
