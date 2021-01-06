046430c1a50784b3c5a6990dca617704af30b95e
Tibo Smet <74244876+TiboSmetKdG@users.noreply.github.com>
2021-01-06 16:07:17 +0100

    Add files via upload

---
+++ Main/Sound_Sensor_Test.ino
@@ -0,0 +1,23 @@
+/*
+    Sound Sensor testing
+
+    Program to test the sound sensor that we will be using for our project.
+
+    The circuit:
+    * Sound sensor
+
+    Created 06 January 2021
+    By Tibo Smet
+    Modified 06 January 2021
+    By Tibo Smet
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
