efce6a552d470bed7e263dde04decf581db5190f
Tibo Smet <74244876+TiboSmetKdG@users.noreply.github.com>
2021-01-08 10:38:18 +0100

    Update LED_Matrix_Test.ino

--- Main/LED_Matrix_Test.ino
+++ Main/LED_Matrix_Test.ino
@@ -1,23 +1,24 @@
 /*
-    LED Matrix testing
+    Sound Sensor testing
 
-    Program to test the LED Matrix that we will be using for our project.
+    Program to test the sound sensor that we will be using for our project.
 
     The circuit:
-    * LED Matrix
+    * Sound sensor
 
     Created 06 January 2021
     By Tibo Smet
-    Modified 06 January 2021
+    Modified 08 January 2021
     By Tibo Smet
 */
 
-void setup() {
-  // put your setup code here, to run once:
+int soundSensor = analogRead(0);           // Connect sound sensor to Analog pin 0
 
+void setup() {
+  Serial.begin(9600);                      // Starts Serial monitor
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  Serial.println(soundSensor, DEC);        //print the sound value to serial
+  delay(100);
 }
