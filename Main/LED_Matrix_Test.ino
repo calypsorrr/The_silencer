d17bc91e2c2197e7976d18498cc507558cb3a7bf
Tibo Smet <74244876+TiboSmetKdG@users.noreply.github.com>
2021-01-08 11:25:47 +0100

    Update LED_Matrix_Test.ino

--- Main/LED_Matrix_Test.ino
+++ Main/LED_Matrix_Test.ino
@@ -1,24 +1,51 @@
 /*
-    Sound Sensor testing
+    LED Matrix testing
 
-    Program to test the sound sensor that we will be using for our project.
+    Program to test the LED Matrix that we will be using for our project.
 
     The circuit:
-    * Sound sensor
+    * LED Matrix
 
     Created 06 January 2021
     By Tibo Smet
-    Modified 08 January 2021
+    Modified 06 January 2021
     By Tibo Smet
 */
 
-int soundSensor = analogRead(0);           // Connect sound sensor to Analog pin 0
+#include <Adafruit_GFX.h>             // Core graphics library
+#include <RGBmatrixPanel.h>           // Library for the LED-Matrix
+
+#define CLK 8
+#define LAT A3
+#define OE  9
+#define A   A0
+#define B   A1
+#define C   A2
+RGBmatrixPanel matrix(A, B, C, CLK, LAT, OE, false);
 
 void setup() {
-  Serial.begin(9600);                      // Starts Serial monitor
+  matrix.begin();
 }
 
 void loop() {
-  Serial.println(soundSensor, DEC);        //print the sound value to serial
+  matrix.fillScreen(matrix.Color333(7, 0, 0));      // Turns the matrix Red
+  delay(100);
+  
+  matrix.fillScreen(matrix.Color333(0, 7, 0));      // Turns the matrix Green
+  delay(100);
+  
+  matrix.fillScreen(matrix.Color333(0, 0, 7));      // Turns the matrix Blue
+  delay(100);
+  
+  matrix.fillScreen(matrix.Color333(7, 7, 0));      // Turns the matrix Yellow
+  delay(100);
+
+  matrix.fillScreen(matrix.Color333(7, 0, 7));      // Turns the matrix Purple
+  delay(100);
+
+  matrix.fillScreen(matrix.Color333(7, 7, 7));      // Turns the matrix white
+  delay(100);
+  
+  matrix.fillScreen(matrix.Color333(0, 0, 0));      // Turns the matrix Black
   delay(100);
 }
