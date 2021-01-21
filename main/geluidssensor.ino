14efb0a3b05f367d4dc03024dc1525b5d26fcf75
stynon <73358457+stynon@users.noreply.github.com>
2021-01-21 13:52:10 +0100

    Update geluidssensor.ino

--- main/geluidssensor.ino
+++ main/geluidssensor.ino
@@ -1,42 +1,59 @@
 /*
     The Silencer
+
     We use a sound sensor to determine if it is to loud. If it is to loud a led light will light up. When the noise is down the led will stay on for 5 more seconds.
+
     The circuit:
       Input:
       Gravity: Analog Sound Sensor For Arduino on analog pin A0
       Output:
       LED light on pin 12
+
     Created 06/01/2021
     By Stijn Janssen
-    Modified 06/01/2021
+    Modified 13/01/2021
     By Stijn Janssen
 */
 
 #include <Chrono.h>
+#include <Adafruit_GFX.h>
+#include <RGBmatrixPanel.h>
+
+#define CLK 8
+#define LAT A3
+#define OE 9
+#define A A0
+#define B A1
+#define C A2
+RGBmatrixPanel matrix(A, B, C, CLK, LAT, OE, false);
 
 int sensor_value = 0;
 int threshold = 100;
 int abs_value = 0;
-int led = 12;
 Chrono chronoA;
 Chrono chronoB;
+Chrono chronoC;
 
 void setup() {
   Serial.begin(9600); // setup serial
-  pinMode(led, OUTPUT);
+  matrix.begin();
 }
 
 void loop() {
-  sensor_value = analogRead(A0);
+  sensor_value = analogRead(A4);
   if (threshold < sensor_value) {
-    digitalWrite(led, HIGH);
+    matrix.fillScreen(matrix.Color333(7, 0, 0));
     chronoA.restart();
   }
   else if (chronoA.hasPassed(5000)) {
-    digitalWrite(led, LOW);
+    matrix.fillScreen(matrix.Color333(0, 7, 0));
     chronoA.stop();
   }
-  if (chronoB.hasPassed(100) || threshold < sensor_value) {
+  if (chronoC.hasPassed(1000) && threshold < sensor_value){
+    Serial.println(sensor_value);
+    chronoC.restart();
+  }
+  else if (chronoB.hasPassed(2000) && threshold > sensor_value){
     chronoB.restart();
     Serial.println(sensor_value);
   }
